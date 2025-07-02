34bf4f7a6726028763c4429b24280565e5fa906b
JaouaO <jaoua.o@gmail.com>
2025-07-02 09:49:57 +0200

    feature/add_enchereService_and_EnchereServiceImpl

---
+++ src/main/java/fr/eni/encheres/bll/EnchereService.java
@@ -0,0 +1,34 @@
+package fr.eni.encheres.bll;
+
+import java.util.List;
+
+import fr.eni.encheres.bo.Article;
+import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.bo.Enchere;
+
+public interface EnchereService {
+
+	void creerArticle(Article article);
+
+	void mettreEnVente(long idArticle);
+
+	void annulerVente(long idArticle);
+
+	void finaliserVente(long idArticle);
+
+	void vendreArticle(long idArticle, Enchere enchere);
+
+	Article consulterParId(long idArticle);
+
+	List<Article> consulterTout();
+
+	List<Article> consulterParRecherche(String recherche);
+
+	List<Article> consulterParCategorie(Categorie categorie);
+
+	List<Article> consulterParEtat(String etat);
+
+	
+	void ajouterEnchere(Enchere enchere);
+	
+}
