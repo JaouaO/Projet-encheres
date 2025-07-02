226cdf4186498d80ea7a3f82932d9d0ed5c068b1
Psen-eni <pi.senard974@gmail.com>
2025-07-02 11:28:26 +0200

    complete/update_enchereDAOImpl

--- src/main/java/fr/eni/encheres/dal/EnchereDAOImpl.java
+++ src/main/java/fr/eni/encheres/dal/EnchereDAOImpl.java
@@ -16,44 +16,44 @@ public class EnchereDAOImpl implements EnchereDAO {
     // ajout d'une enchère
     public List<Enchere> ajouterEnchere(Enchere enchere) {
 
-        String CREATE = "INSERT INTO Enchere (id, date_enchere, montant_enchere, id_utilisateur, id_article) VALUES (:id, :dateEnchere, :montantEnchere, :idUtilisateur, :idArticle)";
+        String creerEchere = "INSERT INTO Enchere (id, date_enchere, montant_enchere, id_utilisateur, id_article) VALUES (:id, :dateEnchere, :montantEnchere, :idUtilisateur, :idArticle)";
 
-        return this.namedParameterJdbcTemplate.query(CREATE, new BeanPropertyRowMapper<>(Enchere.class));
+        return this.namedParameterJdbcTemplate.query(creerEchere, new BeanPropertyRowMapper<>(Enchere.class));
     }
     @Override
 
     // consultation de l'utilisateur par son id
     public List<Enchere> consulterParUtilisateur(long idUtilisateur) {
 
-        String FIND_BY_ID = "SELECT id_utilisateur FROM Enchere WHERE id_utilisateur = :idUtilisateur";
+        String trouverUtilisateur = "SELECT id_utilisateur FROM Enchere WHERE id_utilisateur = :idUtilisateur";
 
-        return this.namedParameterJdbcTemplate.query(FIND_BY_ID, new BeanPropertyRowMapper<>(Enchere.class));
+        return this.namedParameterJdbcTemplate.query(trouverUtilisateur, new BeanPropertyRowMapper<>(Enchere.class));
 
     }
     @Override
     // consultation par article
     public List<Enchere> consulterParArticle(long idArticle) {
 
-        String FIND_BY_ARTICLE = "SELECT id_article FROM Enchere WHERE id_article = :idArticle";
+        String trouverParArticle = "SELECT id_article FROM Enchere WHERE id_article = :idArticle";
 
-        return this.namedParameterJdbcTemplate.query(FIND_BY_ARTICLE, new BeanPropertyRowMapper<>(Enchere.class));
+        return this.namedParameterJdbcTemplate.query(trouverParArticle, new BeanPropertyRowMapper<>(Enchere.class));
 
     }
     @Override
     // consulter la liste des enchères
     public List<Enchere> consulterTout() {
 
-        String FIND_ALL = "SELECT dateEnchere, montantEnchere, utilisateur, article FROM Enchere";
+        String consulTout = "SELECT dateEnchere, montantEnchere, utilisateur, article FROM Enchere";
 
-        return this.namedParameterJdbcTemplate.query(FIND_ALL, new BeanPropertyRowMapper<>(Enchere.class));
+        return this.namedParameterJdbcTemplate.query(consulTout, new BeanPropertyRowMapper<>(Enchere.class));
     }
     @Override
     // annulation d'une enchère
     public int annulerEnchere(Enchere enchere) {
 
-        String CANCEL_BY_ID = "DELETE FROM Enchere WHERE id_enchere = 1";
+        String supprEnchere = "DELETE FROM Enchere WHERE id_enchere = 1";
 
-        return this.namedParameterJdbcTemplate.update(CANCEL_BY_ID, new BeanPropertySqlParameterSource(enchere));
+        return this.namedParameterJdbcTemplate.update(supprEnchere, new BeanPropertySqlParameterSource(enchere));
 
     }
 
